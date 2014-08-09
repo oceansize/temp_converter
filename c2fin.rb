bcaf53447a9829b3028fafe68791cf75d25fcdef
Roi Driscoll <roi.driscoll@gmail.com>
2014-08-09 23:59:33 +0100

    Add reading from file

---
+++ c2fin.rb
@@ -0,0 +1,9 @@
+puts "Reading Celsius temperature value from data file..."
+
+num       = File.read("temp.dat")
+celcius   = num.to_i
+fahrenheit = (celcius * 9 / 5) + 32
+
+puts "The number from file is " + num + " Celsius"
+print "Conversion: "
+puts fahrenheit.to_s + " fahrenheit."
